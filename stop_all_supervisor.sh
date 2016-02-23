020ff1ebadb380bc94f2ba622fce3edf4c8844b0
yaqinzhang <44459965@qq.com>
2016-02-23 14:41:45 +0800

    Update stop_all_supervisor.sh

--- stop_all_supervisor.sh
+++ stop_all_supervisor.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
- then
-        echo "USAGE: stop_all_supervisor file_name"
-        exit 1;
- else
-        hostlist=$1
-        start_all
-        echo "start all finished!";
-fi
-
-
 stop_all()
 {
- for host in `cat hostlist`
+ for host in `cat $hostlist`
  do
         echo HOST $host
         ssh root@$host "su - storm -c jps | grep 'supervisor' | cut -d ' ' -f 1 | xargs kill -9"
  done
  return 0
 }
- stop_all 
- echo "stop all supervisor";
+
+
+if [ $# -eq 0 ]
+ then
+        echo "USAGE: stop_all_supervisor file_name"
+        exit 1;
+ else
+        hostlist=$1
+        stop_all
+        echo "stop all finished!";
+fi
