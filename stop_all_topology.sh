87a4b021727280848449f5af371a4565f7c494d6
yaqinzhang <44459965@qq.com>
2016-02-23 14:42:13 +0800

    Update stop_all_topology.sh

--- stop_all_topology.sh
+++ stop_all_topology.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+stop_all()
+{
+ echo HOST $host
+ while read topology
+ do
+        echo $topology
+        ssh root@$host "su - storm -c '/usr/local/goldmine/storm/default/bin/storm kill $topology'" < /dev/null
+ done < topologies_list
+ return 0
+}
+
 if [ $# -eq 0 ]
  then
         echo "USAGE: stop_all_topology.sh 10.**.**.**"
@@ -9,14 +20,3 @@ if [ $# -eq 0 ]
         stop_all
         echo "Have stoped all topologies";
 fi
-
-stop_all()
-{
- echo HOST $host
- while read topology
- do
-        echo $topology
-        ssh root@$host "su - storm -c /usr/local/goldmine/storm/default/bin/storm kill $topology" < /dev/null
- done < topologies_list
- return 0
-}
