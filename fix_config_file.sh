e6718a4c11ad5da60971f3a76918cebf3c9e3ade
yaqinzhang <44459965@qq.com>
2016-02-23 14:40:11 +0800

    Update fix_config_file.sh

--- fix_config_file.sh
+++ fix_config_file.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+path="/usr/local/goldmine/storm/default/conf"
+change_to_new()
+{
+ for host in `cat $hostlist`
+ do
+        echo HOST $host
+        ssh root@$host "su - storm -lc 'mv $path/storm.yaml $path/storm.yaml-old'"
+        ssh root@$host "su - storm -lc 'mv $path/storm.yaml-bak $path/storm.yaml'"
+ done
+ return 0
+}
+
+
+change_to_old()
+{
+ for host in `cat $hostlist`
+ do
+        echo HOST $host
+        ssh root@$host "su - storm -lc 'mv $path/storm.yaml $path/storm.yaml-bak'"
+        ssh root@$host "su - storm -lc 'mv $path/storm.yaml-old $path/storm.yaml'"
+ done
+ return 0
+}
+
 if [ $# -ne 2 ]
  then
         echo "USAGE: fix_config_file.sh file_name old|new"
@@ -15,26 +39,3 @@ if [ $# -ne 2 ]
         fi
         echo "change configure finished!";
 fi
-
-change_to_new()
-{
- for host in `cat hostlist`
- do
-        echo HOST $host
-        ssh root@$host "su - storm -lc 'mv storm.yaml storm.yaml-old'"
-        ssh root@$host "su - storm -lc 'mv storm.yaml-bak storm.yaml'"
- done
- return 0
-}
-
-
-change_to_old()
-{
- for host in `cat hostlist`
- do
-        echo HOST $host
-        ssh root@$host "su - storm -lc 'mv storm.yaml storm.yaml-bak'"
-        ssh root@$host "su - storm -lc 'mv storm.yaml-old storm.yaml'"
- done
- return 0
-}
