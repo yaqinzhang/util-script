f4e6475867ad14238784d7af2bc6d9037c94eaad
yaqinzhang <44459965@qq.com>
2016-02-23 14:41:12 +0800

    Update start_all_topology.sh

--- start_all_topology.sh
+++ start_all_topology.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
- then
-        echo "USAGE: start_all_topology.sh file_name" 
-        exit 1;
- else
-        host=$1
-        start_all
-        echo "Have stoped all topologies";
-fi
-
 start_all()
 {
  echo HOST $host
  while read topology
  do
         echo $topology
-        ssh root@$host "su - storm -c /usr/local/goldmine/storm/default/bin/$topology" < /dev/null
+        ssh root@$host "su - storm -c '/usr/local/goldmine/storm/default/bin/$topology'" < /dev/null
  done < topologies_script_list
  return 0
 }
+
+if [ $# -eq 0 ]
+ then
+        echo "USAGE: start_all_topology.sh 10.**.**.**" 
+        exit 1;
+ else
+        host=$1
+        start_all
+        echo "Have started all topologies";
+fi
